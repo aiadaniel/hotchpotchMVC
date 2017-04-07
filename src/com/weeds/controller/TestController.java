023e081167c8807d8a37d5fb1c76a20854d01acd
aiadaniel <lxm445460736@gmail.com>
2017-04-07 20:45:08 +0800

    use annotation to define controller

--- src/com/weeds/controller/TestController.java
+++ src/com/weeds/controller/TestController.java
@@ -3,23 +3,25 @@ package com.weeds.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
-public class TestController implements org.springframework.web.servlet.mvc.Controller{
+@Controller
+public class TestController{
 
 	public TestController() {
 
 	}
 
-	//@RequestMapping(value="/test",method=RequestMethod.GET)
+	@RequestMapping(value="/test",method=RequestMethod.GET)
 	public ModelAndView req() {
-		ModelAndView modelAndView = new ModelAndView("/WEB-INF/test.jsp");
+		ModelAndView modelAndView = new ModelAndView("test");
 		modelAndView.addObject("message", "hi gaga");
 		return modelAndView;
 	}
 
-	@Override
 	public ModelAndView handleRequest(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 		ModelAndView modelAndView = new ModelAndView(/*"/WEB-INF/test.jsp"*/);
